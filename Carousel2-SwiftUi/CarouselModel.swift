8c7c4f7bd8c50b66c5097077fcf87ed58a6725e1
mothgharenimish <mothgharenimish0@gmail.com>
2025-05-04 12:06:52 +0530

    Xcoded Updated Changes

---
+++ Carousel2-SwiftUi/CarouselModel.swift
@@ -0,0 +1,8 @@
+//
+//  CarouselModel.swift
+//  Carousel2-SwiftUi
+//
+//  Created by Nimish Mothghare on 03/05/25.
+//
+
+import Foundation
---
+++ Carousel2-SwiftUi/MoviesCard.swift
@@ -0,0 +1,18 @@
+//
+//  MoviesCard.swift
+//  Carousel2-SwiftUi
+//
+//  Created by Nimish Mothghare on 03/05/25.
+//
+
+import SwiftUI
+
+struct MoviesCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoviesCard()
+}
